78ef33b7a68234a787aea66773acf59b73351da0
zaharovmykola <zaharovmykola@gmail.com>
2020-08-17 14:17:47 +0200

    some text deleted

---
+++ src/main/java/org/mykola/zakharov/springboot/react/market/internet/market/service/ProductService.java
@@ -0,0 +1,231 @@
+package org.mykola.zakharov.springboot.react.market.internet.market.service;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.mykola.zakharov.springboot.react.market.internet.market.dao.CategoryHibernateDAO;
+import org.mykola.zakharov.springboot.react.market.internet.market.dao.ProductHibernateDAO;
+import org.mykola.zakharov.springboot.react.market.internet.market.dao.predicate.ProductPredicatesBuilder;
+import org.mykola.zakharov.springboot.react.market.internet.market.entity.Category;
+import org.mykola.zakharov.springboot.react.market.internet.market.entity.Product;
+import org.mykola.zakharov.springboot.react.market.internet.market.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+
+import javax.transaction.Transactional;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class ProductService {
+
+    @Autowired
+    private ProductHibernateDAO productDao;
+
+    @Autowired
+    private CategoryHibernateDAO categoryDao;
+
+    public ResponseModel create(ProductModel productModel) {
+        Optional<Category> categoryOptional
+                = categoryDao.findById(productModel.getCategoryId());
+        if(categoryOptional.isPresent()){
+            Product product =
+                Product.builder()
+                    .name(productModel.getTitle().trim())
+                    .description(productModel.getDescription().trim())
+                    .price(productModel.getPrice())
+                    .quantity(productModel.getQuantity())
+                    .image(productModel.getImage())
+                    .category(categoryOptional.get())
+                    .build();
+            System.out.println(String.format("Product %s Created", product.getName()));
+            productDao.save(product);
+            return ResponseModel.builder()
+                    .status(ResponseModel.SUCCESS_STATUS)
+                    .message(String.format("Product %s Created", product.getName()))
+                    .build();
+        } else {
+            return ResponseModel.builder()
+                .status(ResponseModel.FAIL_STATUS)
+                .message(String.format("Category #%d Not Found", productModel.getCategoryId()))
+                .build();
+        }
+    }
+
+    public ResponseModel update(ProductModel productModel) {
+        Optional<Category> categoryOptional
+                = categoryDao.findById(productModel.getCategoryId());
+        if(categoryOptional.isPresent()){
+            Product product =
+                    Product.builder()
+                            .id(productModel.getId())
+                            .name(productModel.getTitle().trim())
+                            .description(productModel.getDescription().trim())
+                            .price(productModel.getPrice())
+                            .quantity(productModel.getQuantity())
+                            .category(categoryOptional.get())
+                            .build();
+            productDao.save(product);
+            // Demo Logging
+            System.out.println(String.format("Product %s Updated", product.getName()));
+            return ResponseModel.builder()
+                    .status(ResponseModel.SUCCESS_STATUS)
+                    .message(String.format("Product %s Updated", product.getName()))
+                    .build();
+        } else {
+            return ResponseModel.builder()
+                    .status(ResponseModel.FAIL_STATUS)
+                    .message(String.format("Category #%d Not Found", productModel.getCategoryId()))
+                    .build();
+        }
+    }
+
+    public ResponseModel getAll() {
+        List<Product> products = productDao.findAll(Sort.by("id").descending());
+        List<ProductModel> productModels =
+            products.stream()
+                .map(p ->
+                    ProductModel.builder()
+                        .id(p.getId())
+                        .title(p.getName())
+                        .description(p.getDescription())
+                        .price(p.getPrice())
+                        .quantity(p.getQuantity())
+                        .image(p.getImage())
+                        .category(
+                            CategoryModel.builder()
+                                .id(p.getCategory().getId())
+                                .name(p.getCategory().getName())
+                                .build()
+                        )
+                        .build()
+                )
+                .collect(Collectors.toList());
+        return ResponseModel.builder()
+                .status(ResponseModel.SUCCESS_STATUS)
+                .data(productModels)
+                .build();
+    }
+
+    public ResponseModel delete(Long id) {
+        Optional<Product> productOptional = productDao.findById(id);
+        if (productOptional.isPresent()){
+            Product product = productOptional.get();
+            productDao.delete(product);
+            return ResponseModel.builder()
+                .status(ResponseModel.SUCCESS_STATUS)
+                .message(String.format("Product #%s Deleted", product.getName()))
+                .build();
+        } else {
+            return ResponseModel.builder()
+                .status(ResponseModel.FAIL_STATUS)
+                .message(String.format("Product #%d Not Found", id))
+                .build();
+        }
+    }
+
+    public ResponseModel getFiltered(ProductFilterModel filter) {
+        List<Product> products =
+            productDao.findByCategoryIds(
+                filter.categories,
+                Sort.by(filter.sortingDirection, filter.orderBy)
+            );
+        List<ProductModel> productModels =
+            products.stream()
+            .map((p)->
+                ProductModel.builder()
+                    .id(p.getId())
+                    .title(p.getName())
+                    .description(p.getDescription())
+                    .price(p.getPrice())
+                    .quantity(p.getQuantity())
+                    .category(CategoryModel.builder().id(p.getCategory().getId()).name(p.getCategory().getName()).build())
+                    .build()
+            )
+            .collect(Collectors.toList());
+
+        return ResponseModel.builder()
+            .status(ResponseModel.SUCCESS_STATUS)
+            .data(productModels)
+            .build();
+    }
+
+    // поиск отфильтрованного и отсортированного списка товаров
+    // на основе запросов query dsl
+    public ResponseModel search(ProductSearchModel searchModel) {
+        List<Product> products = null;
+        if (searchModel.searchString != null && !searchModel.searchString.isEmpty()) {
+            ProductPredicatesBuilder builder = new ProductPredicatesBuilder();
+            // разбиение значения http-параметра search
+            // на отдельные выражения условий фильтрации
+            Pattern pattern = Pattern.compile("([\\w]+?)(:|<|>|<:|>:)([\\w\\]\\[\\,]+?);");
+            Matcher matcher = pattern.matcher(searchModel.searchString + ";");
+            while (matcher.find()) {
+                builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+            }
+            BooleanExpression expression = builder.build();
+            // выполнение sql-запроса к БД
+            // с набором условий фильтрации
+            // и с указанием имени поля и направления сортировки
+            products =
+                    (List<Product>) productDao.findAll(
+                            expression,
+                            Sort.by(
+                                    searchModel.sortingDirection,
+                                    searchModel.orderBy
+                            )
+                    );
+        } else {
+            products =
+                    productDao.findAll(
+                            Sort.by(
+                                    searchModel.sortingDirection,
+                                    searchModel.orderBy
+                            )
+                    );
+        }
+        return getResponseModelFromEntities(products);
+    }
+
+    private ResponseModel getResponseModelFromEntities(List<Product> products) {
+        List<ProductModel> productModels =
+                products.stream()
+                        .map((p)->
+                                ProductModel.builder()
+                                        .id(p.getId())
+                                        .title(p.getName())
+                                        .description(p.getDescription())
+                                        .price(p.getPrice())
+                                        .quantity(p.getQuantity())
+                                        .image(p.getImage())
+                                        .category(
+                                                CategoryModel.builder()
+                                                        .id(p.getCategory().getId())
+                                                        .name(p.getCategory().getName())
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .collect(Collectors.toList());
+        return ResponseModel.builder()
+                .status(ResponseModel.SUCCESS_STATUS)
+                .data(productModels)
+                .build();
+    }
+
+    public ResponseModel getProductsPriceBounds() {
+        Map<String, Integer> maxndMin = new LinkedHashMap<>();
+        maxndMin.put("min", productDao.findMinimum().intValue());
+        maxndMin.put("max", productDao.findTop1ByOrderByPriceDesc().getPrice().intValue());
+        return ResponseModel.builder()
+                .status(ResponseModel.SUCCESS_STATUS)
+                .data(maxndMin)
+                .build();
+    }
+}
---
+++ src/test/java/org/mykola/zakharov/spring/boot/first/ecommerceshop/junit/service/ProductServiceTest.java
@@ -0,0 +1,282 @@
+package org.mykola.zakharov.spring.boot.first.ecommerceshop.junit.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mykola.zakharov.springboot.react.market.internet.market.dao.CategoryHibernateDAO;
+import org.mykola.zakharov.springboot.react.market.internet.market.dao.ProductHibernateDAO;
+import org.mykola.zakharov.springboot.react.market.internet.market.entity.Category;
+import org.mykola.zakharov.springboot.react.market.internet.market.entity.Product;
+import org.mykola.zakharov.springboot.react.market.internet.market.model.ProductFilterModel;
+import org.mykola.zakharov.springboot.react.market.internet.market.model.ProductModel;
+import org.mykola.zakharov.springboot.react.market.internet.market.model.ResponseModel;
+import org.mykola.zakharov.springboot.react.market.internet.market.service.CategoryService;
+import org.mykola.zakharov.springboot.react.market.internet.market.service.ProductService;
+import org.mykola.zakharov.springboot.react.market.internet.market.service.interfaces.IProductService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.TestPropertySource;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@TestPropertySource(locations = "classpath:application.properties")
+public class ProductServiceTest {
+
+    @Value("tests.unit.strings.image-base64")
+    private String imageBase64;
+
+    @Mock
+    private ProductHibernateDAO productDAO;
+
+    @Mock
+    private CategoryHibernateDAO categoryDAO;
+
+    @Mock
+    private IProductService productServiceMock;
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+    // Внедрение экземпляра службы,
+    // полученного инстанцированием из реализованного класса,
+    // в поля которого внедряются макеты репозиториев и других зависимостей,
+    // если они указаны
+    @InjectMocks
+    private ProductService productService;
+
+    ArgumentCaptor<Product> productArgument =
+            ArgumentCaptor.forClass(Product.class);
+    ArgumentCaptor<Category> categoryArgument =
+            ArgumentCaptor.forClass(Category.class);
+
+    @Test
+    void shouldCreatedProductSuccessfully() {
+        // что вернуть? - объект типа сущность Category
+        doReturn(returnCategoryFoldedInOptional()
+        ).when(categoryDAO) // откуда? - из объекта categoryDAO
+                .findById(1L); // когда? - когда в метод findById передан аргумент 1
+
+        ResponseModel responseModel =
+                productService.create(returnProductModelWithoutId());
+        // Проверка, что результат не равен null
+        assertNotNull(responseModel);
+        // Проверка, что результат содержит положительный статус-код
+        assertEquals(ResponseModel.SUCCESS_STATUS, responseModel.getStatus());
+        // Проверка, что в результате вызванного выше метода (create)
+        // минимум 1 раз был вызван метод save
+        verify(productDAO, atLeast(1))
+                .save(productArgument.capture());
+    }
+
+    @Test
+    void shouldUpdatedProductSuccessfully() {
+        // что вернуть? - объект типа сущность Category
+        doReturn(returnCategoryFoldedInOptional()
+        ).when(categoryDAO) // откуда? - из объекта categoryDAO
+                .findById(1L); // когда? - когда в метод findById передан аргумент 1
+        final ProductModel productModel =
+                ProductModel.builder()
+                        .id(1L)
+                        .title("test product 1")
+                        .description("about test product 1")
+                        .price(new BigDecimal(10.5))
+                        .quantity(5)
+                        .image(imageBase64)
+                        .categoryId(1L)
+                        .build();
+
+        ResponseModel responseModel =
+                productService.create(productModel);
+        // Проверка, что результат не равен null
+        assertNotNull(responseModel);
+        // Проверка, что результат содержит положительный статус-код
+        assertEquals(ResponseModel.SUCCESS_STATUS, responseModel.getStatus());
+        // Проверка, что в результате вызванного выше метода (create)
+        // минимум 1 раз был вызван метод save
+        verify(productDAO, atLeast(1))
+                .save(productArgument.capture());
+    }
+
+    @Test
+    void shouldReturnGetAll() {
+        // Обучаем макет:
+        // вернуть что? - результат, равный ...
+        doReturn(returnListOfProductModels()
+        ).when(productServiceMock) // откуда? - из объекта categoryServiceMock
+                .getAll(); // как результат вызова какого метода? - getAll
+        // вызов настроенного выше метода макета, полученного из интерфейса
+        ResponseModel responseModel =
+                productServiceMock.getAll();
+        assertNotNull(responseModel);
+        assertNotNull(responseModel.getData());
+        assertEquals(((List) responseModel.getData()).size(), 3);
+    }
+
+    @Test
+    void shouldThrowConstraintException() {
+        // строка недопустимой длины
+        final String tooLongProductName =
+                "test category 1234567890 1234567890 1234567890";
+        // модель, содержащая эту строку
+        final ProductModel tooLongNameProductModel =
+                ProductModel.builder().title(tooLongProductName).build();
+        // обучение макета службы, созданного из интерйейса:
+        // дано: когда будет вызван метод create службы,
+        // и ему будет передан аргумент со строкой недопустимой длины -
+        given(productServiceMock.create(tooLongNameProductModel))
+                .willThrow(new IllegalArgumentException()); // нужно выбросить исключение IllegalArgumentException
+        // проверка
+        try {
+            // модель, содержащая ту же слишком длинную строку
+            final ProductModel productModel =
+                    ProductModel.builder()
+                            .title(tooLongProductName)
+                            .build();
+            // попытка выполнить на модели метод с аргументом,
+            // который должен вызвать исключение
+            productServiceMock.create(productModel);
+            // если исключение не выброшено -
+            // объявляем данный тест-кейс не пройденным
+            // с выводом сообщения о причине
+            fail("Should throw an IllegalArgumentException");
+        } catch (IllegalArgumentException ex) {
+        }
+        // после проверяем, был ли хотя бы один раз вызван метод save
+        // с каким-либо аргументом (универсальная заглушка)
+        // на объекте categoryDAO
+        then(productDAO)
+                .should(never())
+                .save(productArgument.capture());
+    }
+
+    @Test
+    // @ExtendWith({SystemOutResource.class, SystemOutResourceParameterResolver.class})
+    @ExtendWith(SystemOutResource.class)
+    void shouldProductCreationSystemOut(/* SystemOutResource sysOut */) {
+        // что вернуть? - объект типа сущность Category
+        doReturn(returnCategoryFoldedInOptional()
+        ).when(categoryDAO) // откуда? - из объекта categoryDAO
+                .findById(1L); // когда? - когда в метод findById передан аргумент 1
+
+        productService.create(returnProductModelWithoutId());
+        assertEquals(
+                String.format("Product %s Created", returnProductModelWithoutId().getTitle().trim()),
+                SystemOutResource.outContent.toString().trim()
+        );
+    }
+
+    // посмотри что еще тестами не сделал
+//     try to descride delete method
+
+    @Test
+    void shouldDeletedProductSuccessfully() {
+        Optional<Product> optionalProduct =
+                Optional.of(
+                        Product.builder()
+                                .id(1L)
+                                .name("test product 1")
+                                .description("about test product 1")
+                                .price(new BigDecimal(10.5))
+                                .quantity(5)
+                                .image(imageBase64)
+                                .build()
+                );
+        doReturn(optionalProduct
+        ).when(productDAO) // откуда? - из объекта categoryDAO
+                .findById(1L); // когда? - когда в метод findById передан аргумент 1
+        final Product product = optionalProduct.get();
+
+        ResponseModel responseModel =
+                productService.delete(product.getId());
+        // Проверка, что результат не равен null
+        assertNotNull(product);
+        // Проверка, что результат содержит положительный статус-код
+        assertEquals(ResponseModel.SUCCESS_STATUS, responseModel.getStatus());
+        // Проверка, что в результате вызванного выше метода (create)
+        // минимум 1 раз был вызван метод save
+        verify(productDAO, atLeast(1))
+                .delete(productArgument.capture());
+    }
+
+    // try to make test for filter
+    @Test
+    void shouldFilteredProductsSuccessfully() {
+        final ProductFilterModel filter =
+                ProductFilterModel.builder()
+                        .categories(Arrays.asList(1L, 2L, 3L))
+                        .orderBy("ASC")
+                        .sortingDirection(Sort.Direction.ASC)
+                        .build();
+        // Обучаем макет:
+        // вернуть что? - результат, равный ...
+        doReturn(returnListOfProductModels()
+        ).when(productServiceMock) // откуда? - из объекта categoryServiceMock
+                .getFiltered(filter); // как результат вызова какого метода? - getAll
+
+        ResponseModel responseModel =
+                productServiceMock.getFiltered(filter);
+        // Проверка, что результат не равен null
+        assertNotNull(responseModel);
+        assertNotNull(responseModel.getData());
+        // Проверка, что результат содержит положительный статус-код
+        assertEquals(ResponseModel.SUCCESS_STATUS, responseModel.getStatus()); // цей може не тра
+        assertEquals(((List) responseModel.getData()).size(), 3);
+    }
+
+    @Test
+    void shouldGetProductsPriceBoundsSuccessfully() {
+        doReturn(new BigDecimal(48.5))
+                .when(productDAO)
+                .findMinimum();
+        doReturn(Product.builder().price(new BigDecimal(200.0)).build())
+                .when(productDAO)
+                .findTop1ByOrderByPriceDesc();
+        ResponseModel responseModel =
+                productService.getProductsPriceBounds();
+        assertNotNull(responseModel);
+        assertNotNull(responseModel.getData());
+        assertEquals(ResponseModel.SUCCESS_STATUS, responseModel.getStatus());
+        verify(productDAO, atLeast(1))
+                .findMinimum();
+        verify(productDAO, atLeast(1))
+                .findTop1ByOrderByPriceDesc();
+    }
+
+    ResponseModel returnListOfProductModels() {
+        return ResponseModel.builder()
+                .status(ResponseModel.SUCCESS_STATUS)
+                .data(Arrays.asList(new ProductModel[]{
+                        ProductModel.builder().
+                                id(1L).title("c1").description("about c1").price(new BigDecimal(10.5)).
+                                quantity(5).image(imageBase64).categoryId(1L).build(),
+                        ProductModel.builder().
+                                id(2L).title("c2").description("about c2").price(new BigDecimal(8.5)).
+                                quantity(7).image(imageBase64).categoryId(2L).build(),
+                        ProductModel.builder().
+                                id(3L).title("c3").description("about c3").price(new BigDecimal(12.5)).
+                                quantity(15).image(imageBase64).categoryId(3L).build()
+                }))
+                .build();
+    }
+
+    Optional<Category> returnCategoryFoldedInOptional() {
+        return Optional.of(Category.builder().id(1L).name("c1").build());
+    }
+
+    ProductModel returnProductModelWithoutId() {
+        return ProductModel.builder()
+                .title("test product 1").description("about test product 1")
+                .price(new BigDecimal(10.5)).quantity(5).image(imageBase64).categoryId(1L).build();
+    }
+
+}
