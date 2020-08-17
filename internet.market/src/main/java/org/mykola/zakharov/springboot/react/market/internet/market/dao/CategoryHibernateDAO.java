30cf524d4d75fb7838de81f5b95df3783f4b327c
zaharovmykola <zaharovmykola@gmail.com>
2020-08-17 13:59:32 +0200

    fixed problems

--- internet.market/src/main/java/org/mykola/zakharov/springboot/react/market/internet/market/dao/CategoryHibernateDAO.java
+++
@@ -1,9 +0,0 @@
-package org.mykola.zakharov.springboot.react.market.internet.market.dao;
-
-import org.mykola.zakharov.springboot.react.market.internet.market.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryHibernateDAO extends JpaRepository<Category, Long> {
-}
--- internet.market/src/main/webapp/app/components/pages/Shopping.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class Shopping extends Component {
-    render () {
-        return <h1>Shopping Page</h1>
-    }
-}
-
-export default Shopping
