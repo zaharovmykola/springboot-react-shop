b697e66f3cf9b0c7d35793f6471277912b7b8532
zaharovmykola <zaharovmykola@gmail.com>
2020-08-27 14:48:32 +0200

    tiny fix ProductModel

--- src/main/webapp/app/models/ProductModel.ts
+++ src/main/webapp/app/models/ProductModel.ts
@@ -5,7 +5,7 @@ export default class ProductModel {
 	public quantity: number
 	public price: number
 	public categoryId: number
-	constructor (title, description, quantity, price, categoryId, id?) {
+	constructor (id?, title?, description?, quantity?, price?, categoryId?) {
 		this.id = id
 		this.title = title
 		this.description = description
