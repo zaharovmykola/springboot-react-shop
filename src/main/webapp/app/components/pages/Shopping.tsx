6a2b4979202a44985b8d7045990e2f7b02e41adc
zaharovmykola <zaharovmykola@gmail.com>
2020-09-27 20:32:46 +0200

    try improve facebook button

--- src/main/webapp/app/components/pages/Shopping.tsx
+++ src/main/webapp/app/components/pages/Shopping.tsx
@@ -394,17 +394,11 @@ class Shopping extends Component<IProps, IState> {
                                         Add to cart
                                     </Button>
                                     <Button
-                                        //className="fb-share-button"
-                                        data-href="https://developers.facebook.com/docs/plugins/"
-                                        data-layout="button_count"
-                                        data-size="large"
                                         className={classes.facebookButton}
                                     >
-
                                         <a
                                             target="_blank"
-                                            href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"
-                                            className="fb-xfbml-parse-ignore"
+                                            href="https://www.facebook.com/sharer/sharer.php?u="
                                         >
                                             Share
                                         </a>
